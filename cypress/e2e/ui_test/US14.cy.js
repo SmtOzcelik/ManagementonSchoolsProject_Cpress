761f573de8cecbd160e3e1adab5bb94e0125b8f6
SmtOzcelik <smtab142790@gmail.com>
2023-10-14 10:54:07 +0300

    10/10/2023

--- cypress/e2e/ui_test/US14.cy.js
+++ cypress/e2e/ui_test/US14.cy.js
@@ -2,16 +2,11 @@
 
 describe('US14:Vice Dean öğretmenleri görüntüleyebilmeli',()=>{
 
-    it.only('TC01 Vice Dean ögretmenliri görür',()=>{
+    it('TC01 Vice Dean ögretmenliri görür',()=>{
 
         cy.Vice_Dean_login_girisi()
         cy.Kullanıcı_teacher_managament_sayfasında()
         cy.Kullanıcı_ogretmenleri_gorunurlugunu_dogrular()
-    })
-    
-    it('',()=>{
-    
-        
     })
     
     
