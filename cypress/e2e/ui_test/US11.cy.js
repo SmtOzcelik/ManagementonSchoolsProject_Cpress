2cb08f0bfcbb66fd612ef332636f5788adef19aa
SmtOzcelik <smtab142790@gmail.com>
2023-10-14 10:54:24 +0300

    10/10/2023

--- cypress/e2e/ui_test/US11.cy.js
+++ cypress/e2e/ui_test/US11.cy.js
@@ -3,6 +3,7 @@
 describe('US11:Vice Dean ders programını görebilmeli',()=>{
 
     it('TC01 Kullanıcı lesson, day, start time, stop time görür',()=>{
+        
         cy.Vice_Dean_login_girisi()
         cy.Kullanıcı_lesson_program_butonuna_tıklar()
         cy.Kullanıcı_lesson_day_starTime_stopTime_gorunurlugunu_dogrular()
