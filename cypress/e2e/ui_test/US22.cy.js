dc5066dd23290b82cdb34fe604a0d5a0917534a9
SmtOzcelik <smtab142790@gmail.com>
2023-10-16 11:42:34 +0300

    10/10/2023

---
+++ cypress/e2e/ui_test/US22.cy.js
@@ -0,0 +1,102 @@
+/// <reference types="cypress" />
+
+describe('US04:Admin ekleme',()=>{
+
+    it('TC01 Pozitif Admin Test',()=>{
+        cy.Admin_login_girisi()
+        cy.Kullanıcı_Name_Surname_BirthPlace_girer('samet','gazi','karaman')
+        cy.Kullanıcı_gender_date_girerr('Male','2010-10-10')
+        cy.Kullanıcı_phone_girer()
+        cy.Kullanıcı_ssn_girer()
+        cy.Kullanıcı_userName_girer()
+        cy.Kullanıcı_password_girer('Ab142700')
+        cy.Kullanıcı_Submit_buton_tıklar()
+        cy.Kullanıcı_kayıt_yapıldıgını_dogrular()
+    })
+    
+    it('TC02 Negatif Test eksik phone number',()=>{
+        cy.Admin_login_girisi()
+        cy.Kullanıcı_Name_Surname_BirthPlace_girer('samet','gazi','karaman')
+        cy.Kullanıcı_gender_date_girerr('Male','2010-10-10')
+        cy.Kullanıcı_yanlıs_phoneNumber_girer('545 545 555')
+        cy.Kullanıcı_ssn_girer()
+        cy.Kullanıcı_userName_girer()
+        cy.Kullanıcı_password_girer('Ab142700')
+        cy.Kullanıcı_Submit_buton_tıklar()
+        cy.Kullanıcı_giris_yapılamadıgını_dogrular_eksikNumberr()
+        
+    })
+    
+    
+    it('TC03 Negatif gecersiz Phone  numarasi',()=>{
+        cy.Admin_login_girisi()
+        cy.Kullanıcı_Name_Surname_BirthPlace_girer('samet','gazi','karaman')
+        cy.Kullanıcı_gender_date_girerr('Male','2010-10-10')
+        cy.Kullanıcı_yanlıs_phoneNumber_girer('545 545 555555')
+        cy.Kullanıcı_ssn_girer()
+        cy.Kullanıcı_userName_girer()
+        cy.Kullanıcı_password_girer('Ab142700')
+        cy.Kullanıcı_Submit_buton_tıklar()
+        cy.Kullanıcı_giris_yapılamadıgını_dogrular_fazlaNumber()
+        
+    })
+    
+    it('TC04 Negatif gecersiz eksik ssn numarasi',()=>{
+        cy.Admin_login_girisi()
+        cy.Kullanıcı_Name_Surname_BirthPlace_girer('samet','gazi','karaman')
+        cy.Kullanıcı_gender_date_girerr('Male','2010-10-10')
+        cy.Kullanıcı_phone_girer()
+        cy.Kullanıcı_eksik_ssn_number_girer('55654845')
+        cy.Kullanıcı_userName_girer()
+        cy.Kullanıcı_password_girer('Ab142700')
+        cy.Kullanıcı_Submit_buton_tıklar()
+        cy.Kullanıcı_giris_yapılamadıgını_dogrular_eksik_ssnNumberr()
+        
+    })
+
+    it('TC05 Negatif  gecersiz Ssn Number',()=>{
+        cy.Admin_login_girisi()
+        cy.Kullanıcı_Name_Surname_BirthPlace_girer('samet','gazi','karaman')
+        cy.Kullanıcı_gender_date_girerr('Male','2010-10-10')
+        cy.Kullanıcı_phone_girer()
+        cy.Kullanıcı_yanlıs_ssn_number_girer('55654845555555')
+        cy.Kullanıcı_userName_girer()
+        cy.Kullanıcı_password_girer('Ab142700')
+        cy.Kullanıcı_Submit_buton_tıklar()
+        cy.Kullanıcı_giris_yapılamadıgını_dogrular_gecersizSsn()
+    })
+
+    it('TC06 Negatif  gecersiz eksik Password',()=>{
+        cy.Admin_login_girisi()
+        cy.Kullanıcı_Name_Surname_BirthPlace_girer('samet','gazi','karaman')
+        cy.Kullanıcı_gender_date_girerr('Male','2010-10-10')
+        cy.Kullanıcı_phone_girer()
+        cy.Kullanıcı_ssn_girer()
+        cy.Kullanıcı_userName_girer()
+        cy.Kullanıcı_password_girer('Ab142')
+        cy.Kullanıcı_Submit_buton_tıklar()
+        cy.Kullanıcı_giris_yapılamadıgını_dogrular_yanlısPassword()
+    })
+    it('TC07 Negatif  gecersiz Büyük harfsiz Password',()=>{
+        cy.Admin_login_girisi()
+        cy.Kullanıcı_Name_Surname_BirthPlace_girer('samet','gazi','karaman')
+        cy.Kullanıcı_gender_date_girerr('Male','2010-10-10')
+        cy.Kullanıcı_phone_girer()
+        cy.Kullanıcı_ssn_girer()
+        cy.Kullanıcı_userName_girer()
+        cy.Kullanıcı_password_girer('ab142000')
+        cy.Kullanıcı_Submit_buton_tıklar()
+        cy.Kullanıcı_giris_yapılamadıgını_dogrular_yanlısPassword()
+    })
+    it('TC08 Negatif  gecersiz küçük harfsiz Password',()=>{
+        cy.Admin_login_girisi()
+        cy.Kullanıcı_Name_Surname_BirthPlace_girer('samet','gazi','karaman')
+        cy.Kullanıcı_gender_date_girerr('Male','2010-10-10')
+        cy.Kullanıcı_phone_girer()
+        cy.Kullanıcı_ssn_girer()
+        cy.Kullanıcı_userName_girer()
+        cy.Kullanıcı_password_girer('AA142000')
+        cy.Kullanıcı_Submit_buton_tıklar()
+        cy.Kullanıcı_giris_yapılamadıgını_dogrular_yanlısPassword()
+    })
+    })
